d0cdffe4714549d41002200d51a99122f0990c14
oTurquoise <turquoisesnake666@gmail.com>
2026-04-19 20:29:17 +0100

    missed these

--- kubejs/client_scripts/common/addedByStarT.js
+++ kubejs/client_scripts/common/addedByStarT.js
@@ -62,7 +62,7 @@ global.addedByStarT = {
         'chemical_plant'
     ],
     subThroughBatch: [
-        'component_nexus', 'component_part_hub', 'super_abs', 'super_ebf', 'super_vacuum_freezer'
+        'component_nexus', 'component_part_hub', 'super_abs', 'super_ebf', 'super_vacuum_freezer', 'super_barrel', 'super_cracker', 'super_pyrolyse', 'super_gas_collector'
     ],
     threadSubBatch: [
         'multithreaded_component_synthesis_forge'
